c3f5d123d0dac7eece79c2f6f9295d7f77c1af64
rafglo <276020@student.pwr.edu.pl>
2024-12-02 19:17:16 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,34 +1,5 @@
 import numpy as np
-
-def generate_matrix(n):
-    mat = np.zeros((n,n))
-    for i in range(n):
-        mat[i][i] = 4
-        if i - 1 >= 0:
-            mat[i][i-1] = -1
-        if i + 1 < n:
-            mat[i][i+1] = -1
-        mat[0][-1] = 1
-        mat[-1][0] = 1
-    return mat
-                     
-
-def ldu(mat):
-    n = len(mat)
-    l, d, u = copy(mat), mat, mat
-    for i in range(n):
-        for j in range(n):
-            if j > i:
-                l[i][j] = 0
-                d[i][j] = 0
-            if j == i:
-                l[i][j] = 0
-                u[i][j] = 0
-            if i > j:
-                u[i][j] = 0
-                d[i][j] = 0
-    return l, d, u 
-
-a = generate_matrix(5)
-
-print(ldu(a))
+x = - 1
+for _ in range(1000):
+    x = np.exp(x) - 1
+print(x)
